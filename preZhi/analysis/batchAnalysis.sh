c9428e0d6fbe97ae513397c71bf177a50fde515b
zhipeng138 <user.name>
2016-10-27 10:35:05 +0800

    do the changes

--- preZhi/analysis/batchAnalysis.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash 
-echo $1 >> www05
-./analysis BA1000-4-9 | sed -n "17p" >> www05
-./analysis BA1000-10 | sed -n "17p" >> www05
-./analysis BA5000-10 | sed -n "17p" >> www05
-./analysis ER1000-5 | sed -n "17p" >> www05
-./analysis ER5000-10 | sed -n "17p" >> www05
