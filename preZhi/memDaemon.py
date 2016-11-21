9b0c459b72c0844610bfd9d6d6f20b20e4bf9111
zhipeng138 <user.name>
2016-11-21 13:04:24 +0800

    .

--- preZhi/memDaemon.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-ISOTIMEFORMAT='%Y-%m-%d %X'
-while(True):
-	#time.sleep(60)
-	print  time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))
-	os.system("ps aux|grep split_")
-	time.sleep(0.1)
